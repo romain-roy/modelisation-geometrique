472db78a0ded18e0d017d4ae968ff0857eeb9931
Romain Roy <rroy.romain@gmail.com>
2019-10-10 10:35:30 +0200

    Cubes

--- Assets/Scripts/Maillage.cs
+++ Assets/Scripts/Maillage.cs
@@ -4,98 +4,108 @@ using UnityEngine;
 
 public class Maillage : MonoBehaviour
 {
-	public Material material;
-	public string nomFichier;
-	public int taille;
+    public Material material;
+    public string nomFichier;
+    public int taille;
+    public bool centrer;
+    public bool normaliserTaille;
 
-	private Vector3[] vertices;
-	private Vector3[] normals;
-	private int[] triangles;
-	private int nbTriangles;
-	private int nbVertices;
+    private Vector3[] vertices;
+    private Vector3[] normals;
+    private int[] triangles;
+    private int nbTriangles;
+    private int nbVertices;
 
-	void Start()
-	{
-		// Création d'un composant MeshFilter qui peut ensuite être visualisé
+    void Start()
+    {
+        // Création d'un composant MeshFilter qui peut ensuite être visualisé
 
-		gameObject.AddComponent<MeshFilter>();
-		gameObject.AddComponent<MeshRenderer>();
+        gameObject.AddComponent<MeshFilter>();
+        gameObject.AddComponent<MeshRenderer>();
 
-		// Lecture du fichier
+        // Lecture du fichier
 
-		string[] reader = System.IO.File.ReadAllLines("Assets/Meshs/" + nomFichier + ".off");
+        string[] reader = System.IO.File.ReadAllLines("Assets/Meshs/" + nomFichier + ".off");
 
-		string[] nombres = reader[1].Split(' ');
+        string[] nombres = reader[1].Split(' ');
 
-		nbVertices = int.Parse(nombres[0]);
-		nbTriangles = int.Parse(nombres[1]) * 3;
+        nbVertices = int.Parse(nombres[0]);
+        nbTriangles = int.Parse(nombres[1]) * 3;
 
-		vertices = new Vector3[nbVertices];
-		normals = new Vector3[nbVertices];
-		triangles = new int[nbTriangles];
+        vertices = new Vector3[nbVertices];
+        normals = new Vector3[nbVertices];
+        triangles = new int[nbTriangles];
 
-		for (int i = 2, v = 0, t = 0; i < reader.Length; i++)
-		{
-			nombres = reader[i].Split(' ');
-			if (i < nbVertices + 2)
-			{
-				vertices[v] = new Vector3(float.Parse(nombres[0].Replace('.', ',')), float.Parse(nombres[1].Replace('.', ',')), float.Parse(nombres[2].Replace('.', ',')));
-				v++;
-			}
-			else
-			{
-				triangles[t] = int.Parse(nombres[1]);
-				triangles[t + 1] = int.Parse(nombres[2]);
-				triangles[t + 2] = int.Parse(nombres[3]);
+        for (int i = 2, v = 0, t = 0; i < reader.Length; i++)
+        {
+            nombres = reader[i].Split(' ');
+            if (i < nbVertices + 2)
+            {
+                vertices[v] = new Vector3(float.Parse(nombres[0].Replace('.', ',')), float.Parse(nombres[1].Replace('.', ',')), float.Parse(nombres[2].Replace('.', ',')));
+                v++;
+            }
+            else
+            {
+                triangles[t] = int.Parse(nombres[1]);
+                triangles[t + 1] = int.Parse(nombres[2]);
+                triangles[t + 2] = int.Parse(nombres[3]);
 
-				// Normales
+                // Normales
 
-				Vector3 edge1 = vertices[triangles[t + 1]] - vertices[triangles[t]];
-				Vector3 edge2 = vertices[triangles[t + 2]] - vertices[triangles[t]];
-				Vector3 normal = Vector3.Normalize(Vector3.Cross(edge1, edge2));
-				normals[triangles[t]] += normal;
-				normals[triangles[t + 1]] += normal;
-				normals[triangles[t + 2]] += normal;
+                Vector3 edge1 = vertices[triangles[t + 1]] - vertices[triangles[t]];
+                Vector3 edge2 = vertices[triangles[t + 2]] - vertices[triangles[t]];
+                Vector3 normal = Vector3.Normalize(Vector3.Cross(edge1, edge2));
+                normals[triangles[t]] += normal;
+                normals[triangles[t + 1]] += normal;
+                normals[triangles[t + 2]] += normal;
 
-				t += 3;
-			}
-		}
+                t += 3;
+            }
+        }
 
-		// Normaliser sa taille
+        // Normaliser sa taille
 
-		float normMax = vertices[0].magnitude;
+        float ratioTaille = 1.0f;
 
-		for (int i = 1; i < nbVertices; i++)
-			if (vertices[i].magnitude > normMax)
-				normMax = vertices[i].magnitude;
+        if (normaliserTaille)
+        {
+            float normMax = vertices[0].magnitude;
 
-		float ratioTaille = normMax / taille;
+            for (int i = 1; i < nbVertices; i++)
+                if (vertices[i].magnitude > normMax)
+                    normMax = vertices[i].magnitude;
 
-		for (int i = 0; i < nbVertices; i++)
-			vertices[i] /= ratioTaille;
+            ratioTaille = normMax / taille;
 
-		// Centrer l'objet
+            for (int i = 0; i < nbVertices; i++)
+                vertices[i] /= ratioTaille;
+        }
 
-		Vector3 sommeVertices = vertices[0];
+        // Centrer l'objet
 
-		for (int i = 1; i < nbVertices; i++)
-			sommeVertices += vertices[i];
+        if (centrer)
+        {
+            Vector3 sommeVertices = vertices[0];
 
-		Vector3 centreGravite = sommeVertices /= nbVertices;
+            for (int i = 1; i < nbVertices; i++)
+                sommeVertices += vertices[i];
 
-		gameObject.transform.position = centreGravite / ratioTaille;
+            Vector3 centreGravite = sommeVertices /= nbVertices;
 
-		// Création et remplissage du Mesh
+            gameObject.transform.position = centreGravite / ratioTaille;
+        }
 
-		Mesh msh = new Mesh();
+        // Création et remplissage du Mesh
 
-		msh.vertices = vertices;
-		msh.triangles = triangles;
-		msh.normals = normals;
+        Mesh msh = new Mesh();
 
-		// Remplissage du Mesh et ajout du material
+        msh.vertices = vertices;
+        msh.triangles = triangles;
+        msh.normals = normals;
 
-		gameObject.GetComponent<MeshFilter>().mesh = msh;
-		gameObject.GetComponent<MeshRenderer>().material = material;
-	}
+        // Remplissage du Mesh et ajout du material
+
+        gameObject.GetComponent<MeshFilter>().mesh = msh;
+        gameObject.GetComponent<MeshRenderer>().material = material;
+    }
 }
--- Assets/Scripts/Volume.cs
+++ Assets/Scripts/Volume.cs
@@ -4,8 +4,20 @@ using UnityEngine;
 
 public class Volume : MonoBehaviour
 {
+    public Transform cube;
+
     void Start()
     {
+        for (int x = 0; x < 10; x++)
+        {
+            for (int y = 0; y < 10; y++)
+            {
+                for (int z = 0; z < 10; z++)
+                {
+                    Instantiate(cube, new Vector3(x, y, z), Quaternion.identity);
+                }
+            }
+        }
 
     }
 }
